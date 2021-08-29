f2bcf707cfadbe0e382cdb03ae6c0d4d573b539d
Yulia Belyakova <yuliya.beliakova@gmail.com>
2021-08-29 18:20:14 +0200

    Add global variable, export server for test

--- write/src/app.ts
+++ write/src/app.ts
@@ -1,16 +1,17 @@
 import express from 'express'
-import mongoose from 'mongoose'
+import { connectDatabase } from './database/database-connection'
+import dotenv from 'dotenv'
+import { router as emissionRoute } from './routes/emissions'
 
-mongoose
-	.connect(
-		'mongodb://db:27017/docker-write',
-		{ useNewUrlParser: true }
-	)
-	.then(() => console.log('MongoDB Connected'))
-	.catch(err => console.log(err));
+dotenv.config()
+
+connectDatabase(process.env.MONGO_ADDRESS);
 
 const app = express()
 
+app.use('/emissions', emissionRoute)
 
 
-app.listen(3000, () => console.log('Write server running'))
+const server = app.listen(process.env.PORT, () => console.log('Write is running'))
+
+export { server }
