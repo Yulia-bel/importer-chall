b52b847038e4bd4ee03c48ecc98c9514d3b5b7fd
Yulia Belyakova <yuliya.beliakova@gmail.com>
2021-08-29 21:13:23 +0200

    Change request url

--- write/src/test/emissions.ts
+++ write/src/test/emissions.ts
@@ -2,7 +2,9 @@ import chai from 'chai'
 import chaiHttp from 'chai-http'
 let should = chai.should()
 
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 chai.use(chaiHttp)
 
@@ -10,6 +12,7 @@ describe('Emissions', () => {
 
 	describe('/POST emissions', () => {
 		it('it should save emissions.csv to database', (done) => {
+
 			let emissions = __dirname + '/../../data/emissions.csv'
 			chai.request('http://localhost:3000')
 				.post('/emissions')
