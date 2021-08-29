0ad3a4a22d553f7e7b97974b888996014b763f72
Yulia Belyakova <yuliya.beliakova@gmail.com>
2021-08-29 18:22:05 +0200

    Add Emissions Schema

---
+++ write/src/models/Emissions.ts
@@ -0,0 +1,512 @@
+import mongoose from 'mongoose'
+
+export const EmissionsSchema = new mongoose.Schema({
+	country: {
+		type: String,
+		required: true
+	},
+	sector: {
+		type: String,
+		required: true
+	},
+	parentSector: {
+		type: String
+	},
+	1850: {
+		type: Number
+	},
+	1851: {
+		type: Number
+	},
+	1852: {
+		type: Number
+	},
+	1853: {
+		type: Number
+	},
+	1854: {
+		type: Number
+	},
+	1855: {
+		type: Number
+	},
+	1856: {
+		type: Number
+	},
+	1857: {
+		type: Number
+	},
+	1858: {
+		type: Number
+	},
+	1859: {
+		type: Number
+	},
+	1860: {
+		type: Number
+	},
+	1861: {
+		type: Number
+	},
+	1862: {
+		type: Number
+	},
+	1863: {
+		type: Number
+	},
+	1864: {
+		type: Number
+	},
+	1865: {
+		type: Number
+	},
+	1866: {
+		type: Number
+	},
+	1867: {
+		type: Number
+	},
+	1868: {
+		type: Number
+	},
+	1869: {
+		type: Number
+	},
+	1870: {
+		type: Number
+	},
+	1871: {
+		type: Number
+	},
+	1872: {
+		type: Number
+	},
+	1873: {
+		type: Number
+	},
+	1874: {
+		type: Number
+	},
+	1875: {
+		type: Number
+	},
+	1876: {
+		type: Number
+	},
+	1877: {
+		type: Number
+	},
+	1878: {
+		type: Number
+	},
+	1879: {
+		type: Number
+	},
+	1880: {
+		type: Number
+	},
+	1881: {
+		type: Number
+	},
+	1882: {
+		type: Number
+	},
+	1883: {
+		type: Number
+	},
+	1884: {
+		type: Number
+	},
+	1885: {
+		type: Number
+	},
+	1886: {
+		type: Number
+	},
+	1887: {
+		type: Number
+	},
+	1888: {
+		type: Number
+	},
+	1889: {
+		type: Number
+	},
+	1890: {
+		type: Number
+	},
+	1891: {
+		type: Number
+	},
+	1892: {
+		type: Number
+	},
+	1893: {
+		type: Number
+	},
+	1894: {
+		type: Number
+	},
+	1895: {
+		type: Number
+	},
+	1896: {
+		type: Number
+	},
+	1897: {
+		type: Number
+	},
+	1898: {
+		type: Number
+	},
+	1899: {
+		type: Number
+	},
+	1900: {
+		type: Number
+	},
+	1901: {
+		type: Number
+	},
+	1902: {
+		type: Number
+	},
+	1903: {
+		type: Number
+	},
+	1904: {
+		type: Number
+	},
+	1905: {
+		type: Number
+	},
+	1906: {
+		type: Number
+	},
+	1907: {
+		type: Number
+	},
+	1908: {
+		type: Number
+	},
+	1909: {
+		type: Number
+	},
+	1910: {
+		type: Number
+	},
+	1911: {
+		type: Number
+	},
+	1912: {
+		type: Number
+	},
+	1913: {
+		type: Number
+	},
+	1914: {
+		type: Number
+	},
+	1915: {
+		type: Number
+	},
+	1916: {
+		type: Number
+	},
+	1917: {
+		type: Number
+	},
+	1918: {
+		type: Number
+	},
+	1919: {
+		type: Number
+	},
+	1920: {
+		type: Number
+	},
+	1921: {
+		type: Number
+	},
+	1922: {
+		type: Number
+	},
+	1923: {
+		type: Number
+	},
+	1924: {
+		type: Number
+	},
+	1925: {
+		type: Number
+	},
+	1926: {
+		type: Number
+	},
+	1927: {
+		type: Number
+	},
+	1928: {
+		type: Number
+	},
+	1929: {
+		type: Number
+	},
+	1930: {
+		type: Number
+	},
+	1931: {
+		type: Number
+	},
+	1932: {
+		type: Number
+	},
+	1933: {
+		type: Number
+	},
+	1934: {
+		type: Number
+	},
+	1935: {
+		type: Number
+	},
+	1936: {
+		type: Number
+	},
+	1937: {
+		type: Number
+	},
+	1938: {
+		type: Number
+	},
+	1939: {
+		type: Number
+	},
+	1940: {
+		type: Number
+	},
+	1941: {
+		type: Number
+	},
+	1942: {
+		type: Number
+	},
+	1943: {
+		type: Number
+	},
+	1944: {
+		type: Number
+	},
+	1945: {
+		type: Number
+	},
+	1946: {
+		type: Number
+	},
+	1947: {
+		type: Number
+	},
+	1948: {
+		type: Number
+	},
+	1949: {
+		type: Number
+	},
+	1950: {
+		type: 'Number'
+	},
+	1951: {
+		type: 'Number'
+	},
+	1952: {
+		type: 'Number'
+	},
+	1953: {
+		type: 'Number'
+	},
+	1954: {
+		type: 'Number'
+	},
+	1955: {
+		type: 'Number'
+	},
+	1956: {
+		type: 'Number'
+	},
+	1957: {
+		type: 'Number'
+	},
+	1958: {
+		type: 'Number'
+	},
+	1959: {
+		type: 'Number'
+	},
+	1960: {
+		type: 'Number'
+	},
+	1961: {
+		type: 'Number'
+	},
+	1962: {
+		type: 'Number'
+	},
+	1963: {
+		type: 'Number'
+	},
+	1964: {
+		type: 'Number'
+	},
+	1965: {
+		type: 'Number'
+	},
+	1966: {
+		type: 'Number'
+	},
+	1967: {
+		type: 'Number'
+	},
+	1968: {
+		type: 'Number'
+	},
+	1969: {
+		type: 'Number'
+	},
+	1970: {
+		type: 'Number'
+	},
+	1971: {
+		type: 'Number'
+	},
+	1972: {
+		type: 'Number'
+	},
+	1973: {
+		type: 'Number'
+	},
+	1974: {
+		type: 'Number'
+	},
+	1975: {
+		type: 'Number'
+	},
+	1976: {
+		type: 'Number'
+	},
+	1977: {
+		type: 'Number'
+	},
+	1978: {
+		type: 'Number'
+	},
+	1979: {
+		type: 'Number'
+	},
+	1980: {
+		type: 'Number'
+	},
+	1981: {
+		type: 'Number'
+	},
+	1982: {
+		type: 'Number'
+	},
+	1983: {
+		type: 'Number'
+	},
+	1984: {
+		type: 'Number'
+	},
+	1985: {
+		type: 'Number'
+	},
+	1986: {
+		type: 'Number'
+	},
+	1987: {
+		type: 'Number'
+	},
+	1988: {
+		type: 'Number'
+	},
+	1989: {
+		type: 'Number'
+	},
+	1990: {
+		type: 'Number'
+	},
+	1991: {
+		type: 'Number'
+	},
+	1992: {
+		type: 'Number'
+	},
+	1993: {
+		type: 'Number'
+	},
+	1994: {
+		type: 'Number'
+	},
+	1995: {
+		type: 'Number'
+	},
+	1996: {
+		type: 'Number'
+	},
+	1997: {
+		type: 'Number'
+	},
+	1998: {
+		type: 'Number'
+	},
+	1999: {
+		type: 'Number'
+	},
+	2000: {
+		type: 'Number'
+	},
+	2001: {
+		type: 'Number'
+	},
+	2002: {
+		type: 'Number'
+	},
+	2003: {
+		type: 'Number'
+	},
+	2004: {
+		type: 'Number'
+	},
+	2005: {
+		type: 'Number'
+	},
+	2006: {
+		type: 'Number'
+	},
+	2007: {
+		type: 'Number'
+	},
+	2008: {
+		type: 'Number'
+	},
+	2009: {
+		type: 'Number'
+	},
+	2010: {
+		type: 'Number'
+	},
+	2011: {
+		type: 'Number'
+	},
+	2012: {
+		type: 'Number'
+	},
+	2013: {
+		type: 'Number'
+	},
+	2014: {
+		type: 'Number'
+	}
+})
+
+export const Emissions = mongoose.model('Emissions', EmissionsSchema);
