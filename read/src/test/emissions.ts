5c2e45764f0c4cc3cc94279ad5223f948a6af116
Yulia Belyakova <yuliya.beliakova@gmail.com>
2021-09-02 12:49:24 +0200

    Implement env variables in tests for endpoint url

--- read/src/test/emissions.ts
+++ read/src/test/emissions.ts
@@ -4,7 +4,7 @@ let should = chai.should()
 
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config({ path: __dirname + '../../env' })
 
 chai.use(chaiHttp)
 
@@ -13,12 +13,12 @@ describe('Emissions', () => {
 	describe('/GET emissions', () => {
 		it('it should get emission docs', (done) => {
 
-			chai.request('http://localhost:4000')
-				.get('/emissions?country=ABW&parentSector=Energy')
+			chai.request(process.env.URL + ':' + process.env.PORT)
+				.get('/emissions?country=ABW&sector=Other&year=1850')
 				.end((err, res) => {
 					res.should.have.status(200)
 					res.body.should.be.a('array')
-					res.body.length.should.be.eql(4)
+					res.body.length.should.be.eql(2)
 					done()
 				})
 		})
--- write/src/test/emissions.ts
+++ write/src/test/emissions.ts
@@ -4,7 +4,7 @@ let should = chai.should()
 
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config({ path: __dirname + '../../env' })
 
 chai.use(chaiHttp)
 
@@ -14,7 +14,7 @@ describe('Emissions', () => {
 		it('it should save emissions.csv to database', (done) => {
 
 			let emissions = __dirname + '/../../data/emissions.csv'
-			chai.request('http://localhost:3000')
+			chai.request(process.env.URL + ':' + process.env.PORT)
 				.post('/emissions')
 				.attach('emissions', emissions)
 				.end((err, res) => {
