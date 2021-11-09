0e7a66956ac4198b212d42eb6bc80f67c14775ef
Yulia Belyakova <yuliya.beliakova@gmail.com>
2021-11-09 20:48:14 +0100

    fix docker default network name

--- go.sh
+++ go.sh
@@ -2,4 +2,4 @@
 
 docker-compose up -d
 
-docker run --network emissions_default --name write-api-test --rm --env API_URL=docker-write --env PORT=3000 emissions_write-api npm run test 
+docker run --network importer-challenge_default --name write-api-test --rm --env API_URL=docker-write --env PORT=3000 emissions_write-api npm run test 
